1e95c913a7c61a3ee8c24ce1f27fcf80c9f1a48b
meganlukes1 <82395818+meganlukes@users.noreply.github.com>
2021-12-17 00:36:00 +0000

    cans of paint exercise

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-def talk(name, location, temp):
-  print(f"hello {name}")
-  print(f"How's the weather in {location}?")
-  cel = int((temp - 32) * 5 / 9)
-  print(f"It is currently {cel} degrees celsius")
-talk("Megan", "Spring Hill", 70)
-talk(temp=46, name="Andy", location="Denver")
+import math
+# def talk(name, location, temp):
+#   print(f"hello {name}")
+#   print(f"How's the weather in {location}?")
+#   cel = int((temp - 32) * 5 / 9)
+#   print(f"It is currently {cel} degrees celsius")
+# talk("Megan", "Spring Hill", 70)
+# talk(temp=46, name="Andy", location="Denver")
+
+#How many cans of paint, round up
+def paint_calc(height, width, cover):
+  paint = math.ceil((height * width) / cover)
+  print(paint)
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
