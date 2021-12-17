3badbcde6097c7bda34f3ef0fd9758985787cafe
meganlukes1 <82395818+meganlukes@users.noreply.github.com>
2021-12-17 14:42:58 +0000

    prime number checker

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import math
+
+#How to instantiate variables
 # def talk(name, location, temp):
 #   print(f"hello {name}")
 #   print(f"How's the weather in {location}?")
@@ -7,11 +9,35 @@ import math
 # talk("Megan", "Spring Hill", 70)
 # talk(temp=46, name="Andy", location="Denver")
 
+
 #How many cans of paint, round up
-def paint_calc(height, width, cover):
-  paint = math.ceil((height * width) / cover)
-  print(paint)
-test_h = int(input("Height of wall: "))
-test_w = int(input("Width of wall: "))
-coverage = 5
-paint_calc(height=test_h, width=test_w, cover=coverage)
+# def paint_calc(height, width, cover):
+#   paint = math.ceil((height * width) / cover)
+#   print(paint)
+# test_h = int(input("Height of wall: "))
+# test_w = int(input("Width of wall: "))
+# coverage = 5
+# paint_calc(height=test_h, width=test_w, cover=coverage)
+
+
+#prime number checker
+# def prime_checker(number):
+#   factors = 0
+#   for digit in range(1, number):
+#     if number % digit == 0:
+#       factors = factors + 1
+#   print(factors)
+#   if factors == 1 or number == 1:
+#     print(f"{number} is a prime number")
+#   else:
+#     print(f"{number} is not a prime number")
+# n = int(input("Check this number: "))
+# prime_checker(number=n)
+
+
+#Caeser Cipher
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
