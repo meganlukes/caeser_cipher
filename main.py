badf00f70de49cbd4df3c1535560e72b0533b9ca
meganlukes1 <82395818+meganlukes@users.noreply.github.com>
2021-12-17 00:29:31 +0000

    How to pass arguments

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def talk(name):
+def talk(name, location, temp):
   print(f"hello {name}")
-  print("wassup")
-  print("yo")
-talk("Megan")
+  print(f"How's the weather in {location}?")
+  cel = int((temp - 32) * 5 / 9)
+  print(f"It is currently {cel} degrees celsius")
+talk("Megan", "Spring Hill", 70)
+talk(temp=46, name="Andy", location="Denver")
